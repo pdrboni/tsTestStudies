edf1e0b9be7c41e1d51b5ff77fd44a8372910602
Pedro Boni Gasparotto <pedrobg2707@gmail.com>
2024-10-20 11:58:06 -0300

    testing persistency

--- src/services/persistency.spec.ts
+++ src/services/persistency.spec.ts
@@ -1,11 +1,28 @@
 import { Persistency } from './persistency';
 
 describe('Persistency', () => {
+
+  afterEach(() => jest.clearAllMocks());
+
   it('should return undefined', () => {
     // sut = System Under Test
     const sut = new Persistency();
     expect(sut.saveOrder()).toBeUndefined();
-  })
+  });
+
+  it('should call console.log once', () => {
+    const sut = new Persistency();
+    const consoleSpy = jest.spyOn(console, 'log');
+    sut.saveOrder();
+    expect(consoleSpy).toHaveBeenCalledTimes(1);
+  });
+
+  it('should call console.log with the string that I want', () => {
+    const sut = new Persistency();
+    const consoleSpy = jest.spyOn(console, 'log');
+    sut.saveOrder();
+    expect(consoleSpy).toHaveBeenCalledWith('Saved with success');
+  });
 })
 
 describe('Testing 1', () => {
